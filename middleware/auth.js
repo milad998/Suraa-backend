70c30860742ed0a0a42c0e85e2c7796a991064f4
milad998 <myaralbwd20@gmail.com>
2025-06-25 01:15:18 +0300

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, 'YOUR_SECRET_KEY');
+    req.user = decoded; // يحتوي على userId أو email حسب التوكن
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+};
