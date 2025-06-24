2d1f728e2e18df8df45dd83187a09cd7fa67f462
milad998 <myaralbwd20@gmail.com>
2025-06-24 20:08:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
