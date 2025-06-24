1c08581638810d1174b278e258b2f237fb58bd49
milad998 <myaralbwd20@gmail.com>
2025-06-25 01:23:45 +0300

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const { storage } = require('../config/cloudinary');
 const upload = multer({ storage });
+const authMiddleware = require('../middleware/auth');
 
 const { sendMessage, getMessages } = require('../controllers/messageController');
 
 const router = express.Router();
 
-router.post('/', upload.single('audio'), sendMessage);
-router.get('/:user1/:user2', getMessages);
+router.post('/',authMiddleware, upload.single('audio'), sendMessage);
+router.get('/:user2',authMiddleware, getMessages);
 
 module.exports = router;
