53b0008893a1455990ed9bf5c6f5608d437f9422
milad998 <myaralbwd20@gmail.com>
2025-06-29 23:24:44 +0300

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth');
-const { sendMessage, getMessages } = require('../controllers/messageController');
+const { sendMessage, getMessages, deleteMessage } = require('../controllers/messageController');
 
 const router = express.Router();
 
 router.post('/', authMiddleware, sendMessage);
 router.get('/:user2', authMiddleware, getMessages);
+router.delete('/:id', authMiddleware, deleteMessage); // ✅ مسار حذف الرسالة
 
 module.exports = router;
