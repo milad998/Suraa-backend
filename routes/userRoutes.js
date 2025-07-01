d809947a2e886bda469ad5ee7a8eae3c5153a094
milad998 <myaralbwd20@gmail.com>
2025-07-01 11:17:31 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createUser, loginUser, getUsers } = require('../controllers/userController');
+const { createUser, loginUser, searchContacts } = require('../controllers/userController');
+const authMiddleware = require('../middleware/auth');
+
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
-router.get('/', getUsers);
+router.post('/search-contacts', authMiddleware, searchContacts); // ✅ جديد
 
 module.exports = router;
