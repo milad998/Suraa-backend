6e9691cf3b5b55cb1676ad93f2dd6ff7b847dfe4
milad998 <myaralbwd20@gmail.com>
2025-07-01 11:08:15 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, required: true }, // جديد
+  username: { type: String, required: true },
+  phone: { type: String, unique: true, required: true }, // ✅ رقم الهاتف
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
