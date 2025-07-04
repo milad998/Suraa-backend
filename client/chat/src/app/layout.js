4b092e890d782958c57717379c18c5babbe9e938
milad998 <myaralbwd20@gmail.com>
2025-07-05 01:49:33 +0300

    Update layout.js

--- client/chat/src/app/layout.js
+++ client/chat/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from './Navbar';
+import Navbar from "./Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
