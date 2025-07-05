6f12646b4fe32482e38f75f3c74bbd53834a127e
milad998 <myaralbwd20@gmail.com>
2025-07-05 17:58:36 +0300

    Create Head.jsx

---
+++ client/chat/src/app/Head.jsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>مشروعي</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+      />
+    </>
+  );
+}
