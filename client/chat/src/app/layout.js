f1c39136768d1bb4cbac1bf1f5c8dbdd66f522fc
milad998 <myaralbwd20@gmail.com>
2025-07-05 18:05:02 +0300

    Update layout.js

--- client/chat/src/app/layout.js
+++ client/chat/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Navbar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,16 +14,26 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Suraa Chat",
-  description: "سرى شات هو تطبيق دردشة آمن وسريع وسهل الاستخدام. تواصل مع من تحب دون قيود وبخصوصية تامة.",
+  description:
+    "سرى شات هو تطبيق دردشة آمن وسريع وسهل الاستخدام. تواصل مع من تحب دون قيود وبخصوصية تامة.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" dir="rtl">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* رابط أيقونات Bootstrap Icons */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar/>
+        <Navbar />
         {children}
       </body>
     </html>
   );
-}
+          }
