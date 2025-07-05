b7b0721bcb49f90601819a912725e37e84a73d7f
milad998 <myaralbwd20@gmail.com>
2025-07-05 18:21:35 +0300

    Update Navbar.jsx

--- client/chat/src/app/Navbar.jsx
+++ client/chat/src/app/Navbar.jsx
@@ -1,8 +1,6 @@
 "use client";
 import { useEffect } from "react";
 
-
-
 export default function Navbar() {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -10,10 +8,7 @@ export default function Navbar() {
 
   return (
     <nav className="navbar navbar-light bg-light sticky-top" dir="rtl">
-      <div className="container d-flex justify-content-between align-items-center">
-        {/* الشعار */}
-        
-
+      <div className="container d-flex justify-content-center align-items-center">
         {/* الأيقونات فقط */}
         <div className="d-flex gap-4">
           <a href="/" className="text-dark fs-4" aria-label="الرئيسية">
